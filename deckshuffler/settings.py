ed26081675037ca6aadb817080a2b5f089c1a7b6
Anil Panda <tsolan3@gmail.com>
2025-04-10 20:24:39 +0400

    fix: allowed hosts

--- deckshuffler/settings.py
+++ deckshuffler/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key'
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deck-shuffler.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
